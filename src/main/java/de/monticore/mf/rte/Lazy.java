d01eb7727da06a48f750a14011732a4bb1eb5e77
Flo Drux <drux@se-rwth.de>
2022-03-25 13:16:01 +0100

    remove personal note

--- src/main/java/de/monticore/mf/rte/Lazy.java
+++ src/main/java/de/monticore/mf/rte/Lazy.java
@@ -9,6 +9,5 @@ import de.monticore.mf.rte.functions.Function0;
  *
  * @param <T> the type to be calculated
  */
-//todo most likely remove later but check with lists
 public interface Lazy<T> extends Function0<T> {
 }

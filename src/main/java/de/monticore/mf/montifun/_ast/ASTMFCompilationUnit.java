01fd98a1e4d89088d63ac7bed351a7b172d730bb
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2022-12-08 15:26:00 +0100

    CompilationUnit::addImportStatement

--- src/main/java/de/monticore/mf/montifun/_ast/ASTMFCompilationUnit.java
+++ src/main/java/de/monticore/mf/montifun/_ast/ASTMFCompilationUnit.java
@@ -1,7 +1,14 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.mf.montifun._ast;
 
+import de.monticore.mf.montifun._parser.MontiFunParser;
+import de.monticore.types.mcbasictypes._ast.ASTMCImportStatement;
 import de.monticore.types.mcbasictypes._ast.ASTMCQualifiedName;
+import de.se_rwth.commons.logging.Log;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Optional;
 
 public class ASTMFCompilationUnit extends ASTMFCompilationUnitTOP {
 
@@ -14,6 +21,28 @@ public class ASTMFCompilationUnit extends ASTMFCompilationUnitTOP {
     ASTMCQualifiedName qName = getMCPackageDeclaration().getMCQualifiedName().deepClone();
     qName.addParts(getMFArtifact().getName());
     return qName.getQName();
+  }
+
+  /**
+   * adds an import statement
+   *
+   * @param importSignature the signature of the import statement to be added
+   */
+  public void addImportStatement(String importSignature) {
+    if (!importSignature.startsWith("import ")) {
+      importSignature = "import " + importSignature;
+    }
+    if (!importSignature.endsWith(";")) {
+      importSignature += ";";
+    }
+    try {
+      Optional<ASTMCImportStatement> importStatementOpt = new MontiFunParser()
+          .parseMCImportStatement(new StringReader(importSignature));
+      this.addMCImportStatement(importStatementOpt.get());
+    }
+    catch (IOException e) {
+      Log.error("0x12002: can't parse '" + importSignature + "': ", e);
+    }
   }
 
 }
